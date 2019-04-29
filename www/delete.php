cddd71a87a5643af7a5fbb9a7c831f6efbff6d52
baguspra <48430327+baguspra@users.noreply.github.com>
2019-04-29 12:50:10 +0700

    Update delete.php

--- www/delete.php
+++ www/delete.php
@@ -1,9 +1,9 @@
 <?php
  include "db.php";
- if(isset($_GET['id_baak']))
+ if(isset($_GET['id']))
  {
- $id_baak=$_GET['id_baak'];
- $q=mysqli_query($con,"delete from `baak` where `id_baak`='$id_baak'");
+ $id=$_GET['id'];
+ $q=mysqli_query($con,"delete from `daftar` where `id`='$id'");
  if($q)
  echo "success";
  else
