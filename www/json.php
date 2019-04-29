19df6a82f2cf49d03b8cb0f10553f126c08dd1e6
baguspra <48430327+baguspra@users.noreply.github.com>
2019-04-29 12:54:23 +0700

    Update json.php

--- www/json.php
+++ www/json.php
@@ -1,7 +1,7 @@
 <?php
 include "db.php";
 $data=array();
-$q=mysqli_query($con,"select * from `baak`");
+$q=mysqli_query($con,"select * from `daftar`");
 while ($row=mysqli_fetch_object($q)){
  $data[]=$row;
 }
