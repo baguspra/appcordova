f428348b1b3a6690a4c98ca1b20ff9cac2c7458f
baguspra <48430327+baguspra@users.noreply.github.com>
2019-04-29 12:48:42 +0700

    Update db.php

--- www/db.php
+++ www/db.php
@@ -1,4 +1,4 @@
 <?php
  header("Access-Control-Allow-Origin: *");
- $con = mysqli_connect("localhost","id8008086_admin","123456","id8008086_admin") or die ("could not connect database");
+ $con = mysqli_connect("localhost","id9445573_admin","123456","id9445573_admin") or die ("could not connect database");
 ?>
